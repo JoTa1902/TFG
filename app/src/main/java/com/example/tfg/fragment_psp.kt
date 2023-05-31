a536b3a4081905c674f3f5dbfe771fe6bbc218d8
josem <josemiii99xd@gmail.com>
2023-05-31 08:01:01 +0200

    beta_1

--- app/src/main/java/com/example/tfg/fragment_psp.kt
+++ app/src/main/java/com/example/tfg/fragment_psp.kt
@@ -1,59 +1,71 @@
 package com.example.tfg
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.*
+import java.io.File
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [fragment_psp.newInstance] factory method to
- * create an instance of this fragment.
- */
 class fragment_psp : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var gameRecyclerView: RecyclerView
+    private lateinit var adapter: GameListAdapter_2
+    private lateinit var database: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_psp, container, false)
+        val view = inflater.inflate(R.layout.fragment_gb, container, false)
+        gameRecyclerView = view.findViewById(R.id.recyclerView)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_psp.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_psp().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Inicializar la base de datos
+        database = FirebaseDatabase.getInstance().reference.child("games")
+
+        // Obtener la lista de archivos de juegos con extensión .gba en la carpeta de descargas
+        val downloadsDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        val gameFileList = downloadsDirectory.listFiles { _, name -> name.endsWith(".iso") }
+
+        // Configurar el RecyclerView y el adaptador
+        adapter = GameListAdapter_2(requireContext(), emptyList())
+        gameRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        gameRecyclerView.adapter = adapter
+
+        // Verificar si se encontraron archivos de juegos en la carpeta de descargas
+        if (gameFileList.isNotEmpty()) {
+            val gameNames = gameFileList.map { file -> file.nameWithoutExtension }
+
+            // Escuchar los cambios en los juegos de la base de datos
+            database.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val games = mutableListOf<Games>()
+                    for (gameSnapshot in dataSnapshot.children) {
+                        val game = gameSnapshot.getValue(Games::class.java)
+                        game?.let {
+                            if (gameNames.contains(game.name)) {
+                                games.add(it)
+                            }
+                        }
+                    }
+
+                    // Actualizar la lista de juegos en el adaptador
+                    adapter.updateGameList(games)
                 }
-            }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Manejar el error, si es necesario
+                }
+            })
+        }
     }
+
 }
